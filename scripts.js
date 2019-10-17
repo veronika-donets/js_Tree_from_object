6537b1c18e54cd1f8afd44d203889e6b012b9945
Veronika D <donets.veronika@yahoo.com>
2019-10-18 00:17:37 +0300

    add readme

---
+++ scripts.js
@@ -0,0 +1,39 @@
+
+let data = {
+  "Fish": {
+    "trout": {},
+    "salmon": {}
+  },
+
+  "Tree": {
+    "Huge": {
+      "sequoia": {},
+      "oak": {}
+    },
+    "Flowering": {
+      "apple tree": {},
+      "magnolia": {}
+    }
+  }
+};
+
+function makeList(data) {
+  const list = document.createElement('ul');
+  for (let key in data) {
+    let item = document.createElement('li');
+    item.textContent = `${key}`;
+
+    if (typeof data[key] === 'object') {
+      item.append(makeList(data[key]));
+    }
+    list.append(item);
+  }
+  return list;
+}
+
+function createTree(container, data) {
+  container.append(makeList(data));
+}
+
+const container = document.getElementById('container');
+createTree(container, data);

6f11819e71abebc3767b68563b8c51dc825e59d1
Veronika D <donets.veronika@yahoo.com>
2019-10-18 11:59:46 +0300

    add readme

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ function makeList(data) {
     let item = document.createElement('li');
     item.textContent = `${key}`;
 
-    if (typeof data[key] === 'object') {
+    if (typeof data[key] === 'object' && data[key] !== null) {
       item.append(makeList(data[key]));
     }
     list.append(item);
